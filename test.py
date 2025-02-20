e3d2b1602da70a5682f7fb51301151d063f6036c
Cristian David Toro Arboleda <89674567+crisToroA@users.noreply.github.com>
2025-02-20 08:37:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
 import unittest
 import main
 
-class rent_text(unittest.TestCase):
-    def normal_text(self):
-        
-        uvt_value = 47_065
-        gross_income = 100_000
+class tests(unittest.TestCase):
+
+    def test_normal_1(self):
+        uvt_value = 47065
+        gross_income = 100_000_000
         costs_deductions = 10_000_000
         exempt_income = 5_000_000
         tax_discounts = 500_000
         withholdings = 1_000_000
         patrimony = 200_000_000
-        rate = 28
+        rate = 0.28
+    
+        resultado_esperado = 22_300_000
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony, rate)
         
-        rate = rate/100
-
-        expected_exit = 22_300_000
-
-        actual_exit = main.calculated_tax()
-
-
-
+        self.assertEqual(calculated_tax, resultado_esperado)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
