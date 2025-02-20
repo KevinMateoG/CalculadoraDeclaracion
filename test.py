881df80dbefad128d1a1d55b62352e521952dcce
Cristian David Toro Arboleda <89674567+crisToroA@users.noreply.github.com>
2025-02-20 09:40:31 -0500

    Update test.py
    Added extraordinary tests, 1 doesn't work

--- test.py
+++ test.py
@@ -18,6 +18,35 @@ class tests(unittest.TestCase):
         calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
         
         self.assertEqual(calculated_tax, expected_result)
+    def test_extraordinary_1(self):
+        uvt_value = 47065
+        gross_income = 500_000_000
+        costs_deductions = 50_000_000
+        exempt_income = 10_000_000
+        tax_discounts = 2_000_000
+        withholdings = 5_000_000
+        patrimony = 1_000_000_000
+    
+        expected_result = 440_000_000, 9_349, 35, 147_000_000, 0
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+        
+        self.assertEqual(calculated_tax, expected_result)
+
+    def test_extraordinary_2(self):
+        uvt_value = 47065
+        gross_income = 120_000_000
+        costs_deductions = 50_000_000
+        exempt_income = 20_000_000
+        tax_discounts = 0
+        withholdings = 500_000
+        patrimony = 200_000_000
+    
+        expected_result = 50_000_000, 1_062, 0, -427_000
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+        
+        self.assertEqual(calculated_tax, expected_result)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
