731097a9436b075e9404f3be5b049e1ef95804a7
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2025-02-27 09:39:58 -0500

    test corregidos y agregado el de error
    Co-authored-by: Cristian David Toro Arboleda <crisToroA@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+class ErrorRentaExenta(Exception):
+    """ ERROR: La renta exenta no puede ser negativa """
+
+
 def tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony):
-    # Calcular Base Gravable
+
+    if exempt_income < 0:
+        raise ErrorRentaExenta()
+
     tax_base = gross_income - costs_deductions - exempt_income
     tax_base_uvt = tax_base / uvt_value if uvt_value else 0
     
-    # Determinar tarifa según la base gravable en UVT (esto puede ajustarse según normativa específica)
-    if tax_base_uvt <= 0:
+    if tax_base_uvt <= 1090:
         rate = 0.0
-    elif tax_base_uvt < 1000:
+    elif tax_base_uvt <= 1700:
         rate = 0.19
-    elif tax_base_uvt < 5000:
+    elif tax_base_uvt <= 4100:
         rate = 0.28
-    else:
+    elif tax_base_uvt <= 8670:
+        rate = 0.33
+    elif tax_base_uvt <= 18970:
         rate = 0.35
+    elif tax_base_uvt <= 31000:
+        rate = 0.37
+    else:
+        rate = 0.39
     
-    # Calcular impuesto
     calculated_tax = (tax_base * rate) - (tax_discounts + withholdings)
     
-    # Determinar saldo a favor
     balance_favor = max(0, withholdings + tax_discounts - calculated_tax)
 
     rate = rate*100
--- test.py
+++ test.py
@@ -11,90 +11,106 @@ class tests(unittest.TestCase):
         tax_discounts = 500_000
         withholdings = 1_000_000
         patrimony = 200_000_000
-        rate = 0.28
     
         expected_result = 85_000_000, 1_806, 28, 22_300_000, 0
 
         calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
         
+        self.assertAlmostEqual(calculated_tax, expected_result)
+        
+    def test_normal_2(self):
+        uvt_value = 47065
+        gross_income = 100_000_000
+        costs_deductions = 0
+        exempt_income = 0
+        tax_discounts = 0
+        withholdings = 1_000_000
+        patrimony = 200_000_000
+
+        expected_result = (100_000_000, 2_125, 28, 27_000_000, 0)
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+
         self.assertEqual(calculated_tax, expected_result)
+
+    def test_normal_3(self):
+        uvt_value = 47065
+        gross_income = 69_718_000
+        costs_deductions = 5_000_000
+        exempt_income = 3_000_000
+        tax_discounts = 500_000
+        withholdings = 1_000_000
+        patrimony = 80_000_000
+
+        expected_result = (61_718_000, 1_311, 19, 10_226_420, 0)
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+
+        self.assertEqual(calculated_tax, expected_result)
+
+    def test_normal_4(self):
+        uvt_value = 47065
+        gross_income = 100_000_000
+        costs_deductions = 95_000_000
+        exempt_income = 0
+        tax_discounts = 200_000
+        withholdings = 300_000
+        patrimony = 150_000_000
+
+        expected_result = (5_000_000, 106, 0, -500_000, 1_000_000)
+
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+
+        self.assertEqual(calculated_tax, expected_result)
+
+
+    def test_extraordinary_1(self):
+        uvt_value = 47065
+        gross_income = 500_000_000
+        costs_deductions = 50_000_000
+        exempt_income = 10_000_000
+        tax_discounts = 2_000_000
+        withholdings = 5_000_000
+        patrimony = 1_000_000_000
         
-def test_normal_2(self):
-    uvt_value = 47065
-    gross_income = 100_000_000
-    costs_deductions = 0
-    exempt_income = 0
-    tax_discounts = 0
-    withholdings = 1_000_000
-    patrimony = 200_000_000
+        expected_result = 440_000_000, 9_349, 35, 147_000_000, 0
 
-    expected_result = (100_000_000, 2_125, 28, 27_000_000, 0)
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+            
+        self.assertEqual(calculated_tax, expected_result)
 
-    calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+    def test_extraordinary_2(self):
+        uvt_value = 47065
+        gross_income = 120_000_000
+        costs_deductions = 50_000_000
+        exempt_income = 20_000_000
+        tax_discounts = 0
+        withholdings = 500_000
+        patrimony = 200_000_000
 
-    self.assertEqual(calculated_tax, expected_result)
+        expected_result = (50_000_000, 1_062, 0, -500_000, 1_000_000)
 
-def test_normal_3(self):
-    uvt_value = 47065
-    gross_income = 69_178_000
-    costs_deductions = 5_000_000
-    exempt_income = 3_000_000
-    tax_discounts = 500_000
-    withholdings = 1_000_000
-    patrimony = 200_000_000
-
-    expected_result = (60_178_000, 1_276, 28, 15_800_000, 0)
-
-    calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
-
-    self.assertEqual(calculated_tax, expected_result)
-
-def test_normal_4(self):
-    uvt_value = 47065
-    gross_income = 100_000_000
-    costs_deductions = 95_000_000
-    exempt_income = 0
-    tax_discounts = 200_000
-    withholdings = 300_000
-    patrimony = 150_000_000
-
-    expected_result = (5_000_000, 106, 28, 1_300_000, 0)
-
-    calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
-
-    self.assertEqual(calculated_tax, expected_result)
-
-
-def test_extraordinary_1(self):
-    uvt_value = 47065
-    gross_income = 500_000_000
-    costs_deductions = 50_000_000
-    exempt_income = 10_000_000
-    tax_discounts = 2_000_000
-    withholdings = 5_000_000
-    patrimony = 1_000_000_000
-    
-    expected_result = 440_000_000, 9_349, 35, 147_000_000, 0
-
-    calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
+        calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
         
-    self.assertEqual(calculated_tax, expected_result)
+        self.assertEqual(calculated_tax, expected_result)
 
-def test_extraordinary_2(self):
-    uvt_value = 47065
-    gross_income = 120_000_000
-    costs_deductions = 50_000_000
-    exempt_income = 20_000_000
-    tax_discounts = 0
-    withholdings = 500_000
-    patrimony = 200_000_000
+    def test_error_1(self):
+        
+        uvt_value = 47065
+        gross_income = 1_000_000
+        costs_deductions = 0
+        exempt_income = -1_000_000
+        tax_discounts = 0
+        withholdings = 0
+        patrimony = 1_000_000
 
-    expected_result = (50_000_000, 1_062, 28, -427_000, 427_000)  # Corregido con 5 elementos
+        try:
+            expected_result = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
 
-    calculated_tax = main.tax_payment(uvt_value, gross_income, costs_deductions, exempt_income, tax_discounts, withholdings, patrimony)
-    
-    self.assertEqual(calculated_tax, expected_result)
+            self.fail("No sirvió bro")
 
+        except main.ErrorRentaExenta as ex:
+            pass
 
 if __name__ == '__main__':
-    unittest.main(exit=False)
+    unittest.main()
