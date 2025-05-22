4d39333d774bad7dcd96e07355e596f5f8f54155
Cristian David Toro Arboleda <89674567+crisToroA@users.noreply.github.com>
2025-05-22 08:29:52 -0500

    feat: Added HandlerException

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# Para las aplicaciones web creadas con Flask, debemos importar siempre el modulo 
 from flask import Flask    
 
-# Para poder servir plantillas HTML desde archivos, es necesario importar el modulo render_template
 from flask import render_template, request
 
 import sys
@@ -9,11 +7,8 @@ sys.path.append( "src" )
 
 from controller.controller_users import ControladorUsuarios
 
-# Flask constructor: crea una variable que nos servirá para comunicarle a Flask
-# la configuración que queremos para nuestra aplicación
 app = Flask(__name__)     
 
-# decorator: se usa para indicar el URL Path por el que se va a invocar nuestra función
 @app.route('/')      
 def index():
     return render_template("index.html")
@@ -27,6 +22,9 @@ def lista_tarjetas():
     usuario = ControladorUsuarios.BuscarPorID( request.args["id_usuario"]  )
     return render_template('lista_tarjetas.html', Id_usuario=request.args["id_usuario"], usuario=usuario  )
 
-# Esta linea permite que nuestra aplicación se ejecute individualmente
+@app.errorhandler(Exception)
+def controlar_errores(err):
+    return "Ocurrió un error mi papacho, sucedió lo siguiente: "  +str(err)
+
 if __name__=='__main__':
    app.run( debug=True)
