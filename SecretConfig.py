cdd583905e91974c21ded329a16074371b5a6dea
Kevin Mateo Gomez <165510156+KevinMateoG@users.noreply.github.com>
2025-05-21 21:51:48 -0500

    Delete SecretConfig.py

--- SecretConfig.py
+++
@@ -1,6 +0,0 @@
-# Do not expose your Neon credentials to the browser
-
-PGHOST='ep-holy-base-a4bgx7y9-pooler.us-east-1.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='npg_Fh0bdQIuep4i'
